05615513ae4f37c11083da6e73ca32fbf91c372c
Bernie <bernie.howard@gmail.com>
2021-11-26 10:50:51 +0200

    changed softmax test to work with a selected layer

--- NeuralNetwork-HW1/NeuralNetwork.py
+++ NeuralNetwork-HW1/NeuralNetwork.py
@@ -123,8 +123,8 @@ class Model:
         dw = grad_softmax_loss_wrt_w(pred, x, y)
         dx = grad_softmax_wrt_x(pred, w, y)
         last_layer.w = last_layer.w - self.lr * dw
-        db = np.sum(dw, axis=1, keepdims=True)
-        last_layer.b = last_layer.b - self.lr * db
+        # db = np.sum(dw, axis=1, keepdims=True)
+        # last_layer.b = last_layer.b - self.lr * db
         for curr_layer in rev_layers[1:]:
             dx = curr_layer.backward(dx)
 
@@ -167,10 +167,11 @@ if __name__ == '__main__':
         {"input_dim": 64, "output_dim": 2, "activation": "softmax"}
     ]
 
-    model = Model(layer_dict=small_arch, batch_size=256, epochs=1, lr=0.1)
+    model = Model(layer_dict=small_arch, batch_size=256, epochs=50, lr=0.1)
 
     datasets = load_datasets('data/*')
     data = datasets['SwissRollData']
     model.train(data)
+
     layers = list(model.get_layers().values())
-    test_grad_softmax_nn(model, data['Yv'], data['Cv'])
+    # test_grad_softmax_nn(model, data['Yv'], data['Cv'])
