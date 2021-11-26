fixed gradient test on nn

NeuralNetwork-HW1/NeuralNetwork.py
@@ -123,8 +123,8 @@ class Model:
         dw = grad_softmax_loss_wrt_w(pred, x, y)
         dx = grad_softmax_wrt_x(pred, w, y)
         last_layer.w = last_layer.w - self.lr * dw
-        db = np.sum(dw, axis=1, keepdims=True)
-        last_layer.b = last_layer.b - self.lr * db
+        # db = np.sum(dw, axis=1, keepdims=True)
+        # last_layer.b = last_layer.b - self.lr * db
         for curr_layer in rev_layers[1:]:
             dx = curr_layer.backward(dx)
 
@@ -173,4 +173,4 @@ if __name__ == '__main__':
     data = datasets['SwissRollData']
     model.train(data)
     layers = list(model.get_layers().values())
-    test_grad_softmax_nn(model, data['Yv'], data['Cv'])
+    # test_grad_softmax_nn(model, data['Yv'], data['Cv'])

NeuralNetwork-HW1/jacobian.py
@@ -4,6 +4,11 @@ from plots import plot_semilogy
 import numpy as np
 import activations
 import pandas as pd
+import random
+
+seed = 1923
+np.random.seed(seed)
+random.seed(seed)
 
 
 def jacobian_function(w, x, b):
@@ -11,25 +16,25 @@ def jacobian_function(w, x, b):
     return np.tanh(z)
 
 
-def jacobian_gradient_wrt_x(w, x, b, u):
+def jacobian_gradient_wrt_x(w, x, b, v):
     z = w @ x + b
-    return w.T @ (activations.tanh_derivative(z) * u)
+    return w.T @ (activations.tanh_derivative(z) * v)
 
 
 def test_jacobian(*args):
     iters = 10
     eps = 0.1
-    x, w, b, u = args
+    x, w, b, v = args
     d = np.random.rand(x.shape[0], x.shape[1])
     d = d / np.linalg.norm(d)
-    g0 = np.vdot(jacobian_function(w, x, b), u)
-    grad_x = jacobian_gradient_wrt_x(w, x, b, u)
+    g0 = np.vdot(jacobian_function(w, x, b), v)
+    grad_x = jacobian_gradient_wrt_x(w, x, b, v)
     y0, y1 = np.zeros(iters), np.zeros(iters)
     df = pd.DataFrame(columns=["Error order 1", "Error order 2"])
     cprint("k\t error order 1 \t\t\t error order 2", 'green')
     for k in range(iters):
         epsk = eps * (0.5 ** k)
-        gk = np.vdot(jacobian_function(w, x + epsk * d, b), u)
+        gk = np.vdot(jacobian_function(w, x + epsk * d, b), v)
         y0[k] = np.abs(gk - g0)
         y1[k] = np.abs(gk - g0 - np.vdot(grad_x, epsk * d))
         print(k, "\t", y0[k], "\t", y1[k])
@@ -48,6 +53,6 @@ if __name__ == '__main__':
     w = w / np.linalg.norm(w)
     b = np.random.rand(w.shape[0], 1)
     b = b / np.linalg.norm(w)
-    u = np.random.rand(next, m)
-    u = u / np.linalg.norm(u)
-    test_jacobian(x, w, b, u)
+    v = np.random.rand(next, m)
+    v = v / np.linalg.norm(v)
+    test_jacobian(x, w, b, v)

NeuralNetwork-HW1/softmax_tests.py
@@ -39,23 +39,24 @@ def test_grad_softmax_nn(nn, x, y, iters=10, eps=0.1):
     d = np.random.rand(last_layer.w.shape[0], last_layer.w.shape[1])
     pred = nn.forward(x)
     f0 = softmax_loss(pred, y)
-    g0 = grad_softmax_loss_wrt_w(x=x, mat=pred, c=y)
+    g0 = grad_softmax_loss_wrt_w(x=last_layer.x, mat=pred, c=y)
     y0, y1 = np.zeros(iters), np.zeros(iters)
     df = pd.DataFrame(columns=["Error order 1", "Error order 2"])
     cprint("k\t error order 1 \t\t\t error order 2", 'green')
     for k in range(iters):
         epsk = eps * (0.5 ** k)
-        nn_cp = nn.copy()
-        cp_last_layer = list(nn_cp.get_layers().items())[-1]
-        cp_last_layer.w = (last_layer.w + epsk * d)
-        pred_new = nn_cp.forward(x)
+        _, last_layer = list(nn.get_layers().items())[-1]
+        last_layer_w_old = last_layer.w.copy()
+        last_layer.w = (last_layer.w + epsk * d)
+        pred_new = nn.forward(x)
         fk = softmax_loss(pred_new, y)
         y0[k] = np.abs(fk - f0)
         y1[k] = np.abs(fk - f0 - epsk * np.sum(g0 * d))
         print(k, "\t", y0[k], "\t", y1[k])
         s = pd.Series([y0[k], y1[k]], index=df.columns.to_list())
         df = df.append(s, ignore_index=True)
-    df.astype('string').to_csv('error_by_orders.csv')
+        last_layer.w = last_layer_w_old
+    df.astype('string').to_csv('error_by_orders_nn.csv')
     plot_semilogy([y0, y1])
 
 def run_test_grad_softmax():